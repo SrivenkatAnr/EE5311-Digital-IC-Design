dff8c90b0fb61e3b131d1c015c6d06e6df0dba31
Srivenkat A <60144334+SrivenkatAnr@users.noreply.github.com>
2021-11-26 22:26:24 +0530

    Pipelining seems to work. Initial error removed. Should check with multiple inputs

---
+++ Project/spice_param_inputs_pulse.py
@@ -0,0 +1,19 @@
+x = int(input("Enter the Multiplicand value: "))
+y = int(input("Enter the Multiplier Value: "))
+
+if x<-128 or x>127 or y<-128 or y>127:
+	raise ValueError("Please enter valid inputs for a 8 bit multiplier!!")
+x_2s = ("{0:0>8}").format(bin(x & int("1"*8, 2))[2:])
+y_2s = ("{0:0>8}").format(bin(y & int("1"*8, 2))[2:])
+
+
+with open("A4_Param_Inputs_Pulse.txt",'w') as f:
+	#for x input----
+	for i in range(8):
+		bit = "{VDD}" if x_2s[i]==str(1) else "0"
+		f.write("v"+str(i+2)+" x"+str(7-i)+" gnd PWL (0 0 {0.1n+Tc} 0 {0.2n+Tc} "+bit+" 2n "+bit+")"+"\n")
+	for i in range(8):
+		bit = "{VDD}" if y_2s[i]==str(1) else "0"
+		f.write("v"+str(i+10)+" y"+str(7-i)+" gnd PWL (0 0 {0.1n+Tc} 0 {0.2n+Tc} "+bit+" 2n "+bit+")"+"\n")
+print("Text File Updated!!")
+
